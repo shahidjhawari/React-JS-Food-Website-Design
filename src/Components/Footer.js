a03f998387660d5de59d6c68f69a44f0a25b03e1
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-12 09:36:27 +0500

    added header & footer components

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+        <p>&copy; 2023 BRAND FOODS - Ali Jan Professional Web Developer</p>
+    </footer>
+  )
+}
---
+++ src/Components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>BRAND FOODS</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="profile.html">Profile</a>
+          </li>
+          <li>
+            <a href="favourit.html">Favourite</a>
+          </li>
+          <li>
+            <a href="about.html">About Us</a>
+          </li>
+          <li>
+            <a href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
