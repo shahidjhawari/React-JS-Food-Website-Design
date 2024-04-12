2f34308a104f7d498cb2d272a94574291328d9da
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-12 10:05:55 +0500

    all images added

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,4 +1,14 @@
-import React from 'react'
+import React from 'react';
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/br1.jpg';
+import img7 from './img/br2.jpg';
+import img8 from './img/br3.jpg';
+import img9 from './img/br4.jpg';
+import img10 from './img/br5.jpg';
 
 function Main() {
   return (
@@ -7,31 +17,31 @@ function Main() {
     <section className="products">
         <div className="product">
             <h3>PIZZA</h3>
-            <img src="img/img1.jpg" alt="Product 1" />
+            <img src={img1} alt="Product 1" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 1200</p>
             <p>Rs. 800</p>
         </div>
         <div className="product">
             <h3>SANDVICH</h3>
-            <img src="img/img2.jpg" alt="Product 2" />
+            <img src={img2} alt="Product 2" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 400</p>
             <p>Rs. 200</p>
         </div>
         <div className="product">
             <h3>BURGER</h3>
-            <img src="img/img3.jpg" alt="Product 3" />
+            <img src={img3} alt="Product 3" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 500</p>
             <p>Rs. 300</p>
         </div>
         <div className="product">
             <h3>HOTDOG</h3>
-            <img src="img/img4.jpg" alt="Product 3" />
+            <img src={img4} alt="Product 3" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 400</p>
             <p>Rs. 350</p>
         </div>
         <div className="product">
             <h3>FRIESES</h3>
-            <img src="img/img5.jpg" alt="Product 3" />
+            <img src={img5} alt="Product 3" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 400</p>
             <p>Rs. 299</p>
         </div>
@@ -41,101 +51,33 @@ function Main() {
     <section className="products">
         <div className="product">
             <h3>CHICKEN BIRYANI</h3>
-            <img src="img/br1.jpg" alt="Product 1" />
+            <img src={img6} alt="Product 1" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 200</p>
             <p>Rs. 150</p>
         </div>
         <div className="product">
             <h3>BHINDI GOSHT</h3>
-            <img src="img/br2.jpg" alt="Product 2" />
+            <img src={img7} alt="Product 2" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 950</p>
             <p>Rs. 800</p>
         </div>
         <div className="product">
             <h3>KOFTA</h3>
-            <img src="img/br3.jpg" alt="Product 3" />
+            <img src={img8} alt="Product 3" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 600</p>
             <p>Rs. 499</p>
         </div>
         <div className="product">
             <h3>KARHI</h3>
-            <img src="img/br4.jpg" alt="Product 3" />
+            <img src={img9} alt="Product 3" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 300</p>
             <p>Rs. 200</p>
         </div>
         <div className="product">
             <h3>CHICKEN KARDAI</h3>
-            <img src="img/br5.jpg" alt="Product 3" />
+            <img src={img10} alt="Product 3" />
             <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 1390</p>
             <p>Rs. 1200</p>
-        </div>
-    </section>
-
-    <h2 className="section-heading">SWEETS</h2>
-    <section className="products">
-        <div className="product">
-            <h3>CHOCOLATE CAKE</h3>
-            <img src="img/sw1.jpg" alt="Product 1" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 450</p>
-            <p>Rs. 300</p>
-        </div>
-        <div className="product">
-            <h3>GULAB JAAMUN</h3>
-            <img src="img/sw2.jpg" alt="Product 2" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 200</p>
-            <p>Rs. 100</p>
-        </div>
-        <div className="product">
-            <h3>BARFI</h3>
-            <img src="img/sw3.jpg" alt="Product 3" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 200</p>
-            <p>Rs. 100</p>
-        </div>
-        <div className="product">
-            <h3>ICE CREAM</h3>
-            <img src="img/sw4.jpg" alt="Product 3" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 400</p>
-            <p>Rs. 320</p>
-        </div>
-        <div className="product">
-            <h3>CHOCOLATE</h3>
-            <img src="img/sw5.jpg" alt="Product 3" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 200</p>
-            <p>Rs. 150</p>
-        </div>
-    </section>
-
-    <h2 className="section-heading">DRINKS</h2>
-    <section className="products">
-        <div className="product">
-            <h3>STING</h3>
-            <img src="img/dri1.png" alt="Product 1" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 150</p>
-            <p>Rs. 130</p>
-        </div>
-        <div className="product">
-            <h3>COKE</h3>
-            <img src="img/dri2.jpg" alt="Product 2" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 120</p>
-            <p>Rs. 70</p>
-        </div>
-        <div className="product">
-            <h3>DEW</h3>
-            <img src="img/dri3.jpg" alt="Product 3" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 120</p>
-            <p>Rs. 70</p>
-        </div>
-        <div className="product">
-            <h3>PEPSI</h3>
-            <img src="img/dri4.jpg" alt="Product 3" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 120</p>
-            <p>Rs. 75</p>
-        </div>
-        <div className="product">
-            <h3>RED BULL</h3>
-            <img src="img/dri5.jpg" alt="Product 3" />
-            <p style={{ textDecorationLine: "line-through", color: "red" }}>Rs. 150</p>
-            <p>Rs. 90</p>
         </div>
     </section>
 </>
